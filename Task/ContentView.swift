4a65848fd0ac7478d5865e53063d8bb21ce87a30
Christian Skorobogatow <christiansko@icloud.com>
2022-07-27 13:58:24 +0200

    Initial Commit

---
+++ Task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Task
+//
+//  Created by Christian Skorobogatow on 27/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by Christian Skorobogatow on 27/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
